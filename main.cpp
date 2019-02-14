9681b30fe37bea1dd9017afbcc952fb4d568de83
TylerHinojosa <46981261+TylerHinojosa@users.noreply.github.com>
2019-02-13 19:48:39 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,20 +1,29 @@
 #include <iostream>
 using namespace std;
 
-bool guess (){
-	cout << "Make your guess:" << endl;
-	return true;
+bool guess (int num){
+	int guess = 0;
+	cout << " Guess a number between 1 and 100: ";
+	cin >> guess;
+		
+	if(guess != num){
+		cout << "You guessed wrong..." << endl;
+		return true;
+	} else {
+		return false;
+	}
 }
 
 int main(){
 	bool failed = true;
-	int number = 73;
 	int count = 0;
+	int number = 73;
 	
 	while (failed){
 		count ++;
-		failed = guess();
+		failed = guess(number);
 	}
 	
+	cout << "You guessed right!!" << endl;
 	cout << "You guessed " << count << " times!" << endl;
 }
